7e722edb24e353b288a5f97c3ae08e80f1023a10
Amanda Munch <amandajmunch@gmail.com>
2021-03-09 12:20:01 -0500

    update readme

--- db/db.js
+++ db/db.js
@@ -1,6 +1,5 @@
 const pgp = require('pg-promise')();
 
-const db = pgp(process.env.DATABASE_URL || 'postgres://postgres:password@localhost:61349/nielsen');
+const db = pgp(process.env.DATABASE_URL || 'postgres://postgres:password@localhost:5432/nielsen');
 
 module.exports = db;
-
